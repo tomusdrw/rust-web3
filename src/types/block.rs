a12c88e0e7f55eceee57563f8763976983ad7377
Robert Habermeier <rphmeier@gmail.com>
2017-01-22 13:46:35 +0100

    tests for changed eth methods

--- src/types/block.rs
+++ src/types/block.rs
@@ -1,14 +1,16 @@
-use serde::{Serialize, Serializer};
-use types::{Bytes, Transaction, U256, H256, H160, H2048};
+use serde::{Serialize, Serializer, Deserialize, Deserializer};
+use types::{Bytes, Transaction, U64, U256, H256, H160, H2048};
 
 /// Block transactions: either hashes or full transactions,
 /// based on flag in request.
-#[derive(Debug, Clone, PartialEq, Deserialize)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum BlockTransactions {
   /// Full transactions.
   Full(Vec<Transaction>),
   /// Transaction hashes.
   Hashes(Vec<H256>),
+  /// No transactions.
+  None,
 }
 
 impl BlockTransactions {
@@ -16,7 +18,7 @@ impl BlockTransactions {
   pub fn full(self) -> Option<Vec<Transaction>> {
     match self {
       BlockTransactions::Full(txs) => Some(txs),
-      BlockTransactions::Hashes(_) => None,
+      _ => None,
     }
   }
 
@@ -24,7 +26,21 @@ impl BlockTransactions {
   pub fn hashes(self) -> Option<Vec<H256>> {
     match self {
       BlockTransactions::Hashes(txs) => Some(txs),
-      BlockTransactions::Full(_) => None,
+      _ => None,
+    }
+  }
+}
+
+impl Deserialize for BlockTransactions {
+  fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
+    where D: Deserializer
+  {
+    let res = Vec::<Transaction>::deserialize(deserializer).map(BlockTransactions::Full)
+      .or_else(|_| Vec::<H256>::deserialize(deserializer).map(BlockTransactions::Hashes));
+    match res {
+      Ok(BlockTransactions::Full(ref v)) if v.is_empty() => Ok(BlockTransactions::None),
+      Ok(BlockTransactions::Hashes(ref v)) if v.is_empty() => Ok(BlockTransactions::None),
+      other => other,
     }
   }
 }
@@ -40,10 +56,9 @@ pub struct Block {
 	/// Hash of the uncles
 	#[serde(rename="sha3Uncles")]
 	pub uncles_hash: H256,
-	/// Authors address
-	pub author: H160,
   /// Miner/author's address.
-  pub miner: H160,
+  #[serde(rename="miner")]
+  pub author: H160,
 	/// State root hash
 	#[serde(rename="stateRoot")]
 	pub state_root: H256,
@@ -54,7 +69,7 @@ pub struct Block {
 	#[serde(rename="receiptsRoot")]
 	pub receipts_root: H256,
 	/// Block number. Null if pending.
-	pub number: Option<u64>,
+	pub number: Option<U64>,
 	/// Gas Used
 	#[serde(rename="gasUsed")]
 	pub gas_used: U256,
