f2acc25e8c2cdb2f6746872cf50a99e004c36bf3
Tomasz Drwięga <tomusdrw@gmail.com>
2017-10-16 19:43:19 +0200

    Update contract deployment example to actually work.

--- examples/contract.rs
+++ examples/contract.rs
@@ -1,8 +1,10 @@
 extern crate web3;
+extern crate rustc_hex;
 
 use web3::futures::Future;
 use web3::contract::{Contract, Options};
 use web3::types::{Address, U256};
+use rustc_hex::FromHex;
 
 fn main() {
   let (_eloop, http) = web3::transports::Http::new("http://localhost:8545").unwrap();
@@ -10,12 +12,17 @@ fn main() {
 
   let my_account: Address = "0x00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap();
   // Get the contract bytecode for instance from Solidity compiler
-  let bytecode = vec![1, 2, 3, 4];
+  let bytecode: Vec<u8> = include_str!("./contract_token.code").from_hex().unwrap();
   // Deploying a contract
   let contract = Contract::deploy(web3.eth(), include_bytes!("../src/contract/res/token.json")).unwrap()
       .confirmations(4)
-      .options(Options::with(|mut opt| opt.value = Some(5.into())))
-      .execute(bytecode, (U256::from(1_000_000), "My Token".to_owned(), 3u64, "MT".to_owned()), my_account)
+      .options(Options::with(|mut opt| opt.gas = Some(5_000_000.into())))
+      .execute(bytecode, (
+          U256::from(1_000_000),
+          "My Token".to_owned(),
+          3u64,
+          "MT".to_owned()
+      ), my_account)
       .expect("Correct parameters are passed to the constructor.")
       .wait()
       .unwrap();
