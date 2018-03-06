566a4d1160de90cd7236fa8a0d64cbe46a0fa0c5
Maxwell Koo <mjkoo90@gmail.com>
2018-03-05 19:55:12 -0800

    Add WebSocket transport

---
+++ examples/simple_ws.rs
@@ -0,0 +1,11 @@
+extern crate web3;
+
+use web3::futures::Future;
+
+fn main() {
+  let (_eloop, ws) = web3::transports::WebSocket::new("ws://localhost:8546").unwrap();
+  let web3 = web3::Web3::new(ws);
+  let accounts = web3.eth().accounts().wait().unwrap();
+
+  println!("Accounts: {:?}", accounts);
+}
