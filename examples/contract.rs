9a8edf8571ea5a177e672489db47d991ac1aa6b6
Frederic Fortier <fredfortier@gmail.com>
2018-07-04 02:14:52 -0700

    Removed 0x prefix from the str address (#132)

--- examples/contract.rs
+++ examples/contract.rs
@@ -10,7 +10,7 @@ fn main() {
     let (_eloop, http) = web3::transports::Http::new("http://localhost:8545").unwrap();
     let web3 = web3::Web3::new(http);
 
-    let my_account: Address = "0x00a329c0648769a73afac7f9381e08fb43dbea72"
+    let my_account: Address = "00a329c0648769a73afac7f9381e08fb43dbea72"
         .parse()
         .unwrap();
     // Get the contract bytecode for instance from Solidity compiler
