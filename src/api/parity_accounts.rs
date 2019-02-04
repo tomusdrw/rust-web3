583c0dac6896e159c8016d12a2be1bbfbc84aa0f
Carlos Perez <37264926+CPerezz@users.noreply.github.com>
2019-02-04 22:50:00 +0100

    Implementing NewAccountFromWallet and removeAddress of parity_accounts API. (#192)
    * Currenly in progress parity_accounts API
    
    * Creating parity_accounts namespace and implementing newAccountFromSecret
    
    * Adding parity_accounts on mod.rs Attaching it to Web3.
    
    * Update src/api/parity_accounts.rs
    
    Fixing grammar
    
    Co-Authored-By: CPerezz <37264926+CPerezz@users.noreply.github.com>
    
    * Fixing nitpicks
    
    * Implement kill_account and new_account_from_phrase
    
    * Implement new account from wallet and remove address
    
    * Correct markdown
    
    * Fixing tests
    
    * Fix raw string input on rpc_test macro

--- src/api/parity_accounts.rs
+++ src/api/parity_accounts.rs
@@ -53,6 +53,25 @@ impl<T: Transport> ParityAccounts<T> {
             .execute("parity_newAccountFromSecret", vec![secret, pwd]),
        ) 
     }
+    /// Imports an account from a JSON encoded Wallet file.
+    /// Returns the address of the corresponding wallet.
+    pub fn parity_new_account_from_wallet(&self, wallet: &str, pwd: &str) -> CallFuture<Address, T::Out> {
+       let wallet = helpers::serialize(&wallet);
+       let pwd = helpers::serialize(&pwd);
+       CallFuture::new(
+           self.transport
+            .execute("parity_newAccountFromWallet", vec![wallet, pwd]),
+       ) 
+    }
+    /// Removes the address of the Parity node addressbook.
+    /// Returns true if the operation suceeded.
+    pub fn parity_remove_address(&self, address: &Address) -> CallFuture<bool, T::Out> {
+       let address = helpers::serialize(&address);
+       CallFuture::new(
+           self.transport
+            .execute("parity_removeAddress", vec![address]),
+       ) 
+    }
 }
 
 #[cfg(test)]
@@ -81,5 +100,17 @@ mod tests {
         ParityAccounts :   new_account_from_secret,  &H256::from("c6592108cc3577f6a2d6178bc6947b43db39057195802caa0120f26e39af4945"), "123456789"
         => "parity_newAccountFromSecret", vec![r#""0xc6592108cc3577f6a2d6178bc6947b43db39057195802caa0120f26e39af4945""#, r#""123456789""#];
         Value::String("0x9b776Baeaf3896657A9ba0db5564623B3E0173e0".into()) => Address::from("0x9b776Baeaf3896657A9ba0db5564623B3E0173e0")
+    );
+
+    rpc_test! (
+        ParityAccounts :   parity_new_account_from_wallet,  r#"{"version":3,"id":"3b330c3b-b0b3-4e39-b62e-c2041a98d673","address":"4c8ab9d3e938285776d6717d7319f6a9b1d809dd","Crypto":{"ciphertext":"bb3a6dbf21f0bf2b5eb0b43426590f16650acee9462ab710cca18781691a5739","cipherparams":{"iv":"6a533f77fc5cb8a752a16ec6a3200da1"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"a58609853dec53c81feb165e346c700e714285771825bb4cbf87c4ea1996b682","n":8192,"r":8,"p":1},"mac":"a71edeb659ed628db13579ce9f75c80c9d386c1239b280548d9a0e58ad20d6c7"}}"#, "123456789"
+        => "parity_newAccountFromWallet", vec![r#""{\"version\":3,\"id\":\"3b330c3b-b0b3-4e39-b62e-c2041a98d673\",\"address\":\"4c8ab9d3e938285776d6717d7319f6a9b1d809dd\",\"Crypto\":{\"ciphertext\":\"bb3a6dbf21f0bf2b5eb0b43426590f16650acee9462ab710cca18781691a5739\",\"cipherparams\":{\"iv\":\"6a533f77fc5cb8a752a16ec6a3200da1\"},\"cipher\":\"aes-128-ctr\",\"kdf\":\"scrypt\",\"kdfparams\":{\"dklen\":32,\"salt\":\"a58609853dec53c81feb165e346c700e714285771825bb4cbf87c4ea1996b682\",\"n\":8192,\"r\":8,\"p\":1},\"mac\":\"a71edeb659ed628db13579ce9f75c80c9d386c1239b280548d9a0e58ad20d6c7\"}}""#, r#""123456789""#];
+        Value::String("0x4C8aB9d3e938285776d6717d7319F6a9B1d809DD".into()) => Address::from("0x4C8aB9d3e938285776d6717d7319F6a9B1d809DD")
+    );
+
+    rpc_test! (
+        ParityAccounts :   parity_remove_address,  &Address::from("0x9b776baeaf3896657a9ba0db5564623b3e0173e0")
+        => "parity_removeAddress", vec![r#""0x9b776baeaf3896657a9ba0db5564623b3e0173e0""#];
+        Value::Bool(true) => true
     );
 }
