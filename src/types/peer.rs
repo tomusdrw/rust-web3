90a18de898a95f80c114e95ff39007d007a275f5
blanksteer <ubiqfree@protonmail.ch>
2019-02-12 15:37:17 -0600

    added missing documentation to nodetype and peertype

--- src/types/peer.rs
+++ src/types/peer.rs
@@ -1,56 +1,51 @@
+//! Types for getting peer information
 use ethereum_types::U256;
 use serde_derive::{Deserialize, Serialize};
 
-use std::fmt;
-
-#[derive(Serialize, Deserialize, Clone)]
+/// Stores active peer count, connected count, max connected peers
+/// and a list of peers
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct PeerType {
+    /// number of active peers
     pub active: usize,
+    /// number of connected peers
     pub connected: usize,
+    /// maximum number of peers that can connect
     pub max: u32,
+    /// list of all peers with details
     pub peers: Vec<PeerInfo>,
 }
 
-impl fmt::Debug for PeerType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "active: {}, connected: {}, max: {}, peers: {:?}", self.active, self.connected, self.max, self.peers)
-
-    }
-}
-
-
-#[derive(Serialize, Deserialize, Clone)]
+/// details of a peer
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct PeerInfo {
+    /// id of peer
     pub id: Option<String>,
+    /// name of peer if set by user
     pub name: String,
+    /// sync logic for protocol messaging
     pub caps: Vec<String>,
+    /// remote address and local address
     pub network: PeerNetworkInfo,
+    /// protocol version of peer
     pub protocols: PeerProtocolsInfo,
 }
-impl fmt::Debug for PeerInfo {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "id: {:?}, name: {}, caps: {:?}, remote address: {}, local address: {}", self.id, self.name, self.caps, self.network.remote_address, self.network.local_address)
-    }
-}
 
-
-#[derive(Serialize, Clone, Deserialize, Debug)]
+/// ip address of both local and remote
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all="camelCase")]
 pub struct PeerNetworkInfo {
-
-    #[serde(rename="remoteAddress")]
     pub remote_address: String,
-
-    #[serde(rename="localAddress")]
     pub local_address: String,
 }
 
-#[derive(Serialize, Clone, Deserialize, Debug)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct PeerProtocolsInfo {
     pub eth: Option<EthProtocolInfo>,
     pub pip: Option<PipProtocolInfo>,
 }
 
-#[derive(Serialize, Clone, Deserialize, Debug)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct EthProtocolInfo {
     pub version: u32,
     pub difficulty: Option<U256>,
