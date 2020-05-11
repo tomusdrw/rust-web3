74b35fa2b0f23529cd7e4c99151c41b5fe39cdbc
Felix Leupold <felixleupold90@gmail.com>
2020-05-11 18:38:01 +0200

    Make logs_bloom optional

--- src/types/block.rs
+++ src/types/block.rs
@@ -86,7 +86,7 @@ pub struct Block<TX> {
     pub extra_data: Bytes,
     /// Logs bloom
     #[serde(rename = "logsBloom")]
-    pub logs_bloom: H2048,
+    pub logs_bloom: Option<H2048>,
     /// Timestamp
     pub timestamp: U256,
     /// Difficulty
