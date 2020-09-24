6710c9f731fe51236966ee797cee187df84d724e
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2020-09-24 15:30:56 +0200

    Update src/types/bytes_array.rs

--- src/types/bytes_array.rs
+++ src/types/bytes_array.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
-/// Array of bytes wrapper
+/// A wrapper type for array of bytes.
+/// 
+/// Implements `Tokenizable` so can be used to retrieve data from `Solidity` contracts returning `byte8[]`.
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Eq, Hash, Serialize)]
 pub struct BytesArray(pub Vec<u8>);
