1818294ff3a790932cfdb8941499f1ede2116a02
Echo Nolan <echo@echonolan.net>
2020-12-07 02:00:54 -0800

    Add EIP-1193 support (#414)
    * Put signing behind an optional feature
    
    libsecp256k1 doesn't compile with the WASM target, and you don't need to sign
    transactions or messages in the browser anyway, since your wallet usually
    manages the keys.
    
    * Add EIP-1193 support
    
    * Add eth_requestAccounts method
    
    * 'cargo check' EIP-1193 code in Travis
    
    * Clean up the feature gates a little
    
    * Style cleanup in eip_1193.rs
    
    * remove meaningless #[wasm_bindgen] attribute
    
    * Refactor translating events from JS
    
    * Replace some Error::Internals with panics with useful messages
    
    * rename get_default -> default
    
    * Refactor error handling in request_wrapped
    
    * clean up EIP-1193 listeners on Drop
    
    * Add support for more EIP-1193 events
    
    * Reduce the number of #[cfg(feature = "signing")] attributes a bit more

--- src/signing.rs
+++ src/signing.rs
@@ -1,11 +1,6 @@
 //! Signing capabilities and utilities.
 
-use crate::types::{Address, H256};
-use secp256k1::recovery::{RecoverableSignature, RecoveryId};
-use secp256k1::{Message, PublicKey, Secp256k1};
-use std::ops::Deref;
-
-pub(crate) use secp256k1::SecretKey;
+use crate::types::H256;
 
 /// Error during signing.
 #[derive(Debug, derive_more::Display, PartialEq, Clone)]
@@ -28,83 +23,136 @@ pub enum RecoveryError {
 }
 impl std::error::Error for RecoveryError {}
 
-/// A trait representing ethereum-compatible key with signing capabilities.
-///
-/// The purpose of this trait is to prevent leaking `secp256k1::SecretKey` struct
-/// in stack or memory.
-/// To use secret keys securely, they should be wrapped in a struct that prevents
-/// leaving copies in memory (both when it's moved or dropped). Please take a look
-/// at:
-/// - https://github.com/graphprotocol/solidity-bindgen/blob/master/solidity-bindgen/src/secrets.rs
-/// - or https://crates.io/crates/zeroize
-/// if you care enough about your secrets to be used securely.
-///
-/// If it's enough to pass a reference to `SecretKey` (lifetimes) than you can use `SecretKeyRef`
-/// wrapper.
-pub trait Key {
-    /// Sign given message and include chain-id replay protection.
+#[cfg(feature = "signing")]
+pub use feature_gated::*;
+
+#[cfg(feature = "signing")]
+mod feature_gated {
+    use super::*;
+
+    use secp256k1::recovery::{RecoverableSignature, RecoveryId};
+    pub(crate) use secp256k1::SecretKey;
+    use secp256k1::{Message, PublicKey, Secp256k1};
+    use std::ops::Deref;
+
+    use crate::types::Address;
+
+    /// A trait representing ethereum-compatible key with signing capabilities.
     ///
-    /// When a chain ID is provided, the `Signature`'s V-value will have chain relay
-    /// protection added (as per EIP-155). Otherwise, the V-value will be in
-    /// 'Electrum' notation.
-    fn sign(&self, message: &[u8], chain_id: Option<u64>) -> Result<Signature, SigningError>;
+    /// The purpose of this trait is to prevent leaking `secp256k1::SecretKey` struct
+    /// in stack or memory.
+    /// To use secret keys securely, they should be wrapped in a struct that prevents
+    /// leaving copies in memory (both when it's moved or dropped). Please take a look
+    /// at:
+    /// - https://github.com/graphprotocol/solidity-bindgen/blob/master/solidity-bindgen/src/secrets.rs
+    /// - or https://crates.io/crates/zeroize
+    /// if you care enough about your secrets to be used securely.
+    ///
+    /// If it's enough to pass a reference to `SecretKey` (lifetimes) than you can use `SecretKeyRef`
+    /// wrapper.
+    pub trait Key {
+        /// Sign given message and include chain-id replay protection.
+        ///
+        /// When a chain ID is provided, the `Signature`'s V-value will have chain relay
+        /// protection added (as per EIP-155). Otherwise, the V-value will be in
+        /// 'Electrum' notation.
+        fn sign(&self, message: &[u8], chain_id: Option<u64>) -> Result<Signature, SigningError>;
 
-    /// Get public address that this key represents.
-    fn address(&self) -> Address;
-}
-
-/// A `SecretKey` reference wrapper.
-///
-/// A wrapper around `secp256k1::SecretKey` reference, which enables it to be used in methods expecting
-/// `Key` capabilities.
-pub struct SecretKeyRef<'a> {
-    key: &'a SecretKey,
-}
-
-impl<'a> SecretKeyRef<'a> {
-    /// A simple wrapper around a reference to `SecretKey` which allows it to be usable for signing.
-    pub fn new(key: &'a SecretKey) -> Self {
-        Self { key }
-    }
-}
-
-impl<'a> From<&'a SecretKey> for SecretKeyRef<'a> {
-    fn from(key: &'a SecretKey) -> Self {
-        Self::new(key)
-    }
-}
-
-impl<'a> Deref for SecretKeyRef<'a> {
-    type Target = SecretKey;
-
-    fn deref(&self) -> &Self::Target {
-        &self.key
-    }
-}
-
-impl<T: Deref<Target = SecretKey>> Key for T {
-    fn sign(&self, message: &[u8], chain_id: Option<u64>) -> Result<Signature, SigningError> {
-        let message = Message::from_slice(&message).map_err(|_| SigningError::InvalidMessage)?;
-        let (recovery_id, signature) = Secp256k1::signing_only()
-            .sign_recoverable(&message, self)
-            .serialize_compact();
-
-        let standard_v = recovery_id.to_i32() as u64;
-        let v = if let Some(chain_id) = chain_id {
-            // When signing with a chain ID, add chain replay protection.
-            standard_v + 35 + chain_id * 2
-        } else {
-            // Otherwise, convert to 'Electrum' notation.
-            standard_v + 27
-        };
-        let r = H256::from_slice(&signature[..32]);
-        let s = H256::from_slice(&signature[32..]);
-
-        Ok(Signature { v, r, s })
+        /// Get public address that this key represents.
+        fn address(&self) -> Address;
     }
 
-    fn address(&self) -> Address {
-        secret_key_address(self)
+    /// A `SecretKey` reference wrapper.
+    ///
+    /// A wrapper around `secp256k1::SecretKey` reference, which enables it to be used in methods expecting
+    /// `Key` capabilities.
+    pub struct SecretKeyRef<'a> {
+        pub(super) key: &'a SecretKey,
+    }
+
+    impl<'a> SecretKeyRef<'a> {
+        /// A simple wrapper around a reference to `SecretKey` which allows it to be usable for signing.
+        pub fn new(key: &'a SecretKey) -> Self {
+            Self { key }
+        }
+    }
+
+    impl<'a> From<&'a SecretKey> for SecretKeyRef<'a> {
+        fn from(key: &'a SecretKey) -> Self {
+            Self::new(key)
+        }
+    }
+
+    impl<'a> Deref for SecretKeyRef<'a> {
+        type Target = SecretKey;
+
+        fn deref(&self) -> &Self::Target {
+            &self.key
+        }
+    }
+
+    impl<T: Deref<Target = SecretKey>> Key for T {
+        fn sign(&self, message: &[u8], chain_id: Option<u64>) -> Result<Signature, SigningError> {
+            let message = Message::from_slice(&message).map_err(|_| SigningError::InvalidMessage)?;
+            let (recovery_id, signature) = Secp256k1::signing_only()
+                .sign_recoverable(&message, self)
+                .serialize_compact();
+
+            let standard_v = recovery_id.to_i32() as u64;
+            let v = if let Some(chain_id) = chain_id {
+                // When signing with a chain ID, add chain replay protection.
+                standard_v + 35 + chain_id * 2
+            } else {
+                // Otherwise, convert to 'Electrum' notation.
+                standard_v + 27
+            };
+            let r = H256::from_slice(&signature[..32]);
+            let s = H256::from_slice(&signature[32..]);
+
+            Ok(Signature { v, r, s })
+        }
+
+        fn address(&self) -> Address {
+            secret_key_address(self)
+        }
+    }
+
+    /// Recover a sender, given message and the signature.
+    ///
+    /// Signature and `recovery_id` can be obtained from `types::Recovery` type.
+    pub fn recover(message: &[u8], signature: &[u8], recovery_id: i32) -> Result<Address, RecoveryError> {
+        let message = Message::from_slice(message).map_err(|_| RecoveryError::InvalidMessage)?;
+        let recovery_id = RecoveryId::from_i32(recovery_id).map_err(|_| RecoveryError::InvalidSignature)?;
+        let signature =
+            RecoverableSignature::from_compact(&signature, recovery_id).map_err(|_| RecoveryError::InvalidSignature)?;
+        let public_key = Secp256k1::verification_only()
+            .recover(&message, &signature)
+            .map_err(|_| RecoveryError::InvalidSignature)?;
+
+        Ok(public_key_address(&public_key))
+    }
+
+    /// Gets the address of a public key.
+    ///
+    /// The public address is defined as the low 20 bytes of the keccak hash of
+    /// the public key. Note that the public key returned from the `secp256k1`
+    /// crate is 65 bytes long, that is because it is prefixed by `0x04` to
+    /// indicate an uncompressed public key; this first byte is ignored when
+    /// computing the hash.
+    pub(crate) fn public_key_address(public_key: &PublicKey) -> Address {
+        let public_key = public_key.serialize_uncompressed();
+
+        debug_assert_eq!(public_key[0], 0x04);
+        let hash = keccak256(&public_key[1..]);
+
+        Address::from_slice(&hash[12..])
+    }
+
+    /// Gets the public address of a private key.
+    pub(crate) fn secret_key_address(key: &SecretKey) -> Address {
+        let secp = Secp256k1::signing_only();
+        let public_key = PublicKey::from_secret_key(&secp, key);
+        public_key_address(&public_key)
     }
 }
 
@@ -116,44 +164,6 @@ pub struct Signature {
     pub r: H256,
     /// S component of the signature.
     pub s: H256,
-}
-
-/// Recover a sender, given message and the signature.
-///
-/// Signature and `recovery_id` can be obtained from `types::Recovery` type.
-pub fn recover(message: &[u8], signature: &[u8], recovery_id: i32) -> Result<Address, RecoveryError> {
-    let message = Message::from_slice(message).map_err(|_| RecoveryError::InvalidMessage)?;
-    let recovery_id = RecoveryId::from_i32(recovery_id).map_err(|_| RecoveryError::InvalidSignature)?;
-    let signature =
-        RecoverableSignature::from_compact(&signature, recovery_id).map_err(|_| RecoveryError::InvalidSignature)?;
-    let public_key = Secp256k1::verification_only()
-        .recover(&message, &signature)
-        .map_err(|_| RecoveryError::InvalidSignature)?;
-
-    Ok(public_key_address(&public_key))
-}
-
-/// Gets the address of a public key.
-///
-/// The public address is defined as the low 20 bytes of the keccak hash of
-/// the public key. Note that the public key returned from the `secp256k1`
-/// crate is 65 bytes long, that is because it is prefixed by `0x04` to
-/// indicate an uncompressed public key; this first byte is ignored when
-/// computing the hash.
-pub(crate) fn public_key_address(public_key: &PublicKey) -> Address {
-    let public_key = public_key.serialize_uncompressed();
-
-    debug_assert_eq!(public_key[0], 0x04);
-    let hash = keccak256(&public_key[1..]);
-
-    Address::from_slice(&hash[12..])
-}
-
-/// Gets the public address of a private key.
-pub(crate) fn secret_key_address(key: &SecretKey) -> Address {
-    let secp = Secp256k1::signing_only();
-    let public_key = PublicKey::from_secret_key(&secp, key);
-    public_key_address(&public_key)
 }
 
 /// Compute the Keccak-256 hash of input bytes.
---
+++ src/transports/eip_1193.rs
@@ -0,0 +1,302 @@
+//! EIP-1193 transport
+//!
+//! This transport lets you use the library inside a browser to interact with
+//! EIP-1193 providers like MetaMask. It's intended for use with Rust's
+//! WebAssembly target.
+
+use crate::api::SubscriptionId;
+use crate::types::{Address, U256};
+use crate::{error, DuplexTransport, Error, RequestId, Transport};
+use futures::channel::mpsc;
+use futures::future::LocalBoxFuture;
+use futures::Stream;
+use jsonrpc_core::types::request::{Call, MethodCall};
+use serde::de::value::StringDeserializer;
+use serde::de::IntoDeserializer;
+use serde::Deserialize;
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+use std::rc::Rc;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+type Subscriptions = Rc<RefCell<BTreeMap<SubscriptionId, mpsc::UnboundedSender<serde_json::Value>>>>;
+
+/// EIP-1193 transport
+#[derive(Clone, Debug)]
+pub struct Eip1193 {
+    provider_and_listeners: Rc<RefCell<ProviderAndListeners>>,
+    subscriptions: Subscriptions,
+}
+
+impl Eip1193 {
+    /// Build an EIP-1193 transport.
+    pub fn new(provider: Provider) -> Self {
+        let mut provider_and_listeners = ProviderAndListeners {
+            provider,
+            listeners: BTreeMap::new(),
+        };
+        let subscriptions: Subscriptions = Subscriptions::default();
+        let subscriptions_for_closure = subscriptions.clone();
+        let msg_handler = Closure::wrap(Box::new(move |evt_js: JsValue| {
+            let evt = evt_js.into_serde::<MessageEvent>().expect("Couldn't parse event data");
+            log::trace!("Message from provider: {:?}", evt);
+            match evt.event_type.as_str() {
+                "eth_subscription" => {
+                    let subscriptions_map = subscriptions_for_closure.borrow();
+                    match subscriptions_map.get(&SubscriptionId::from(evt.data.subscription.clone())) {
+                        Some(sink) => {
+                            if let Err(err) = sink.unbounded_send(evt.data.result) {
+                                log::error!("Error sending notification: {}", err)
+                            }
+                        }
+                        None => log::warn!("Got message for non-existent subscription {}", evt.data.subscription),
+                    }
+                }
+                other => log::warn!("Got unknown notification type: {}", other),
+            }
+        }) as Box<dyn FnMut(JsValue)>);
+        provider_and_listeners.on("message", msg_handler);
+        Eip1193 {
+            provider_and_listeners: Rc::new(RefCell::new(provider_and_listeners)),
+            subscriptions,
+        }
+    }
+
+    /// Get a `Stream` of `connect` events, and the `chainId`s connected to. NB the delivery on these
+    /// is unreliable, sometimes you get one on page load and sometimes you don't. The `Stream` will
+    /// be closed when the `Eip1193` is dropped.
+    pub fn connect_stream(&self) -> impl Stream<Item = Option<String>> {
+        self.handle_ad_hoc_event("connect", |evt_js| {
+            let evt = evt_js
+                .into_serde::<ConnectEvent>()
+                .expect("couldn't parse connect event");
+            evt.chain_id
+        })
+    }
+
+    /// Get a `Stream` of `disconnect` events, with the associated errors. Same drop behavior as
+    /// above.
+    pub fn disconnect_stream(&self) -> impl Stream<Item = jsonrpc_core::Error> {
+        self.handle_ad_hoc_event("disconnect", |evt_js| {
+            evt_js.into_serde().expect("deserializing disconnect error failed")
+        })
+    }
+
+    /// Get a `Stream` of `chainChanged` events, with the `chainId`s changed to. Same drop behavior
+    /// as above.
+    pub fn chain_changed_stream(&self) -> impl Stream<Item = U256> {
+        self.handle_ad_hoc_event("chainChanged", |evt_js| {
+            Self::deserialize_js_string(&evt_js, "chain changed event")
+        })
+    }
+
+    /// Get a `Stream` of `accountsChanged` events, with the addresses that are now accessible. Same
+    /// drop behavior as above.
+    pub fn accounts_changed_stream(&self) -> impl Stream<Item = Vec<Address>> {
+        self.handle_ad_hoc_event("accountsChanged", |evt_js| {
+            js_sys::Array::unchecked_from_js(evt_js)
+                .iter()
+                .map(|js_str| Self::deserialize_js_string(&js_str, "account address"))
+                .collect()
+        })
+    }
+
+    /// Helper function for handling events other than `message`. Given the event name and a
+    /// processor function, create a stream of processed events.
+    fn handle_ad_hoc_event<T, F>(&self, name: &str, handler: F) -> impl Stream<Item = T>
+    where
+        F: Fn(JsValue) -> T + 'static,
+        T: 'static,
+    {
+        let (sender, receiver) = mpsc::unbounded();
+        self.provider_and_listeners.borrow_mut().on(
+            name,
+            Closure::wrap(Box::new(move |evt| {
+                let evt_parsed = handler(evt);
+                if let Err(err) = sender.unbounded_send(evt_parsed) {
+                    log::error!("Couldn't send ad hoc event to channel: {}", err)
+                }
+            })),
+        );
+        receiver
+    }
+
+    /// Helper function for serde-deserializing from `JsString`s. Panics on failure.
+    fn deserialize_js_string<'de, O: Deserialize<'de>>(js_str: &JsValue, name: &str) -> O {
+        let deserializer: StringDeserializer<serde::de::value::Error> = js_str
+            .as_string()
+            .expect(&format!("{:?} not a string", js_str))
+            .into_deserializer();
+        O::deserialize(deserializer).expect(&format!("couldn't deserialize {}", name))
+    }
+}
+
+/// Event data sent from the JavaScript side to our callback.
+#[derive(serde::Deserialize, Debug)]
+struct MessageEvent {
+    #[serde(rename = "type")]
+    event_type: String,
+    data: MessageEventData,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct MessageEventData {
+    subscription: String,
+    result: serde_json::Value,
+}
+
+#[derive(serde::Deserialize)]
+struct ConnectEvent {
+    #[serde(rename = "chainId")]
+    chain_id: Option<String>,
+}
+
+impl Transport for Eip1193 {
+    type Out = LocalBoxFuture<'static, error::Result<serde_json::value::Value>>;
+
+    fn prepare(&self, method: &str, params: Vec<serde_json::Value>) -> (RequestId, Call) {
+        // EIP-1193 uses the JSON-RPC function API, but it isn't actually JSON-RPC, so some of
+        // these fields are ignored.
+        (
+            0,
+            Call::from(MethodCall {
+                jsonrpc: None,
+                method: String::from(method),
+                params: jsonrpc_core::types::Params::Array(params),
+                id: jsonrpc_core::types::Id::Null,
+            }),
+        )
+    }
+
+    fn send(&self, _id: RequestId, request: Call) -> Self::Out {
+        match request {
+            Call::MethodCall(MethodCall {
+                params: jsonrpc_core::types::Params::Array(params),
+                method,
+                ..
+            }) => {
+                let js_params =
+                    js_sys::Array::from(&JsValue::from_serde(&params).expect("couldn't send method params via JSON"));
+                let copy = self.provider_and_listeners.borrow().provider.clone();
+                Box::pin(async move {
+                    copy.request_wrapped(RequestArguments {
+                        method,
+                        params: js_params,
+                    })
+                    .await
+                })
+            }
+            _ => panic!("Can't send JSON-RPC requests other than method calls with EIP-1193 transport!"),
+        }
+    }
+}
+
+impl DuplexTransport for Eip1193 {
+    type NotificationStream = mpsc::UnboundedReceiver<serde_json::Value>;
+
+    fn subscribe(&self, id: SubscriptionId) -> error::Result<Self::NotificationStream> {
+        let (sender, receiver) = mpsc::unbounded();
+        let mut subscriptions_ref = self.subscriptions.borrow_mut();
+        subscriptions_ref.insert(id, sender);
+        Ok(receiver)
+    }
+
+    fn unsubscribe(&self, id: SubscriptionId) -> error::Result<()> {
+        match (*self.subscriptions.borrow_mut()).remove(&id) {
+            Some(_sender) => Ok(()),
+            None => panic!("Tried to unsubscribe from non-existent subscription. Did we already unsubscribe?"),
+        }
+    }
+}
+
+#[wasm_bindgen]
+// Rustfmt removes the 'async' keyword from async functions in extern blocks. It's fixed
+// in rustfmt 2.
+#[rustfmt::skip]
+extern "C" {
+    #[derive(Clone, Debug)]
+    /// An EIP-1193 provider object. Available by convention at `window.ethereum`
+    pub type Provider;
+
+    #[wasm_bindgen(catch, method)]
+    async fn request(_: &Provider, args: RequestArguments) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(method)]
+    fn on(_: &Provider, eventName: &str, listener: &Closure<dyn FnMut(JsValue)>);
+
+    #[wasm_bindgen(method, js_name = "removeListener")]
+    fn removeListener(_: &Provider, eventName: &str, listener: &Closure<dyn FnMut(JsValue)>);
+}
+
+impl Provider {
+    /// Get the provider at `window.ethereum`.
+    pub fn default() -> Result<Self, JsValue> {
+        get_provider_js()
+    }
+
+    async fn request_wrapped(&self, args: RequestArguments) -> error::Result<serde_json::value::Value> {
+        let js_result = self.request(args).await;
+        let parsed_value = js_result.map(|res| res.into_serde()).map_err(|err| err.into_serde());
+        match parsed_value {
+            Ok(Ok(res)) => Ok(res),
+            Err(Ok(err)) => Err(Error::Rpc(err)),
+            err => unreachable!("Unable to parse request response: {:?}", err),
+        }
+    }
+}
+
+/// Keep the provider and the event listeners attached to it together so we can remove them in the
+/// `Drop` implementation. The logic can't go in Eip1193 because it's `Clone`, and cloning a JS
+/// object just clones the reference.
+#[derive(Debug)]
+struct ProviderAndListeners {
+    provider: Provider,
+    listeners: BTreeMap<String, Vec<Closure<dyn FnMut(JsValue)>>>,
+}
+
+impl ProviderAndListeners {
+    /// Listen for an event, and keep the listener closure for later cleanup.
+    fn on(&mut self, name: &str, listener: Closure<dyn FnMut(JsValue)>) {
+        self.provider.on(name, &listener);
+        self.listeners
+            .entry(name.to_owned())
+            .or_insert(Vec::with_capacity(1))
+            .push(listener);
+    }
+}
+
+impl Drop for ProviderAndListeners {
+    fn drop(&mut self) {
+        for (event_name, listeners) in self.listeners.iter() {
+            for listener in listeners.iter() {
+                self.provider.removeListener(event_name, listener)
+            }
+        }
+    }
+}
+
+#[wasm_bindgen(inline_js = "export function get_provider_js() {return window.ethereum}")]
+extern "C" {
+    #[wasm_bindgen(catch)]
+    fn get_provider_js() -> Result<Provider, JsValue>;
+}
+
+#[wasm_bindgen]
+struct RequestArguments {
+    method: String,
+    params: js_sys::Array,
+}
+
+#[wasm_bindgen]
+impl RequestArguments {
+    #[wasm_bindgen(getter)]
+    pub fn method(&self) -> String {
+        self.method.clone()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn params(&self) -> js_sys::Array {
+        self.params.clone()
+    }
+}
