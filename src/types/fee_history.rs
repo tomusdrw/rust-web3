2e9432a6646503aae145b1470ace7aeef7190e82
Dusan Stanivukovic <dusan.stanivukovic@gmail.com>
2021-10-21 10:51:02 +0200

    Apply suggestions from code review
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>

--- src/types/fee_history.rs
+++ src/types/fee_history.rs
@@ -7,11 +7,11 @@ use serde::{Deserialize, Serialize};
 pub struct FeeHistory {
     /// Lowest number block of the returned range.
     pub oldest_block: BlockNumber,
-    /// Base fee per gas
+    /// A vector of block base fees per gas. This includes the next block after the newest of the returned range, because this value can be derived from the newest block. Zeroes are returned for pre-EIP-1559 blocks.
     pub base_fee_per_gas: Vec<U256>,
-    /// Gas used ratio
+    /// A vector of block gas used ratios. These are calculated as the ratio of gas used and gas limit.
     pub gas_used_ratio: Vec<f64>,
-    /// Reward
+    /// A vector of effective priority fee per gas data points from a single block. All zeroes are returned if the block is empty. Returned only if requested.
     pub reward: Option<Vec<Vec<U256>>>,
 }
 
